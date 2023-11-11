345d48f55bcf9287c46b40e9dad0b1d1da95d616
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-11 16:51:36 +0000

    hot fix

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 	{
 		if (!format || (format[0] == '%' && !format[1]))
 			return (-1);
-		if (format[0] == '%' && format[1] == ' ' && !format)
+		if (format[0] == '%' && format[1] == ' ' && !format[2])
 			return (-1);
 		if (format[i] != '%')
 		{
