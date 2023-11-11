1d06464fab00b41918e70d2a702f752cbb47132f
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-11 16:48:23 +0000

    complete implementation of %c, %s and %%

--- _printf.c
+++ _printf.c
@@ -16,6 +16,8 @@ int _printf(const char *format, ...)
 	{
 		if (!format || (format[0] == '%' && !format[1]))
 			return (-1);
+		if (format[0] == '%' && format[1] == ' ' && !format)
+			return (-1);
 		if (format[i] != '%')
 		{
 			len += _putchar(format[i]);
