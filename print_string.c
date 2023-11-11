59e7664d03e30ebbc4745cdab36069c25cb58d52
Nawfal Sakirda <nawfalsakirda@gmail.com>
2023-11-11 08:24:00 -0800

    tweaking print_string.c

--- print_string.c
+++ print_string.c
@@ -9,6 +9,11 @@
 
 int print_string(va_list *ag, int *i)
 {
+	char *s = va_arg(*ag, char *);
+
 	(*i)++;
-	return (_puts(va_arg(*ag, char *)));
+
+	if (s == NULL)
+		return (_puts("(null)"));
+	return (_puts(s));
 }
